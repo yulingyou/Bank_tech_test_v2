7b6962f8d4dad992c58369f27916916b7d9e0c21
Daisy <ysiad926@gmail.com>
2023-01-26 13:32:26 +0000

    Printer second test and drive

--- lib/printer.rb
+++ lib/printer.rb
@@ -1,6 +1,15 @@
 class Printer
-  def printing_bank_statement(bank_statement)
-    return header +  " #{bank_statement.date} || #{bank_statement.credit} || #{bank_statement.debit} || #{bank_statement.balance}"
+  def  initialize
+    @bank_statements = []
+  end
+
+  def printing_bank_statement
+    puts header
+    @bank_statements.each { |statement| puts "#{statement.date} || #{statement.credit} || #{statement.debit} || #{statement.balance}"}
+  end
+
+  def add_bank_statement(bank_statement)
+    @bank_statements << bank_statement
   end
 
   def header
--- spec/printer_spec.rb
+++ spec/printer_spec.rb
@@ -4,6 +4,16 @@ RSpec.describe Printer do
   it "should print correct format" do
     fake_bank_statement = double(:fake_bank_statement, date:'10/01/2023', credit:'1000.00', debit:'', balance:'1000.00')
     printer = Printer.new
-    expect(printer.printing_bank_statement(fake_bank_statement)).to eq("date || credit || debit || balance \n 10/01/2023 || 1000.00 ||  || 1000.00")
+    printer.add_bank_statement(fake_bank_statement)
+    expect(printer.printing_bank_statement).to eq([fake_bank_statement])
+  end
+
+  it "should print correct format with two transaction" do
+    fake_bank_statement_1 = double(:fake_bank_statement, date:'10/01/2023', credit:'1000.00', debit:'', balance:'1000.00')
+    fake_bank_statement_2 = double(:fake_bank_statement, date:'08/01/2023', credit:'1000.00', debit:'', balance:'1000.00')
+    printer = Printer.new
+    printer.add_bank_statement(fake_bank_statement_1)
+    printer.add_bank_statement(fake_bank_statement_2)
+    expect(printer.printing_bank_statement).to eq([fake_bank_statement_1,fake_bank_statement_2])
   end
 end
