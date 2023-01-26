6016aec06fa8d2012cb1f5ee4eb42d7fb56366e3
Daisy <ysiad926@gmail.com>
2023-01-26 23:01:39 +0000

    Bank class should printing correct format with three bank statement

--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -22,10 +22,17 @@ RSpec.describe Bank do
     expect(bank.balance).to eq(2500)
   end
 
-  it "should printing correct bank statement with one bank statement(transaction) " do
+  it "should printing correct bank statement format with one bank statement(transaction) " do
     bank = Bank.new
-    bank.create_bank_statement('10/01/2023',nil,1000, 1000)
-    expect{bank.printing_final_statement}.to output("date || credit || debit || balance \n10/01/2023 ||  || 1000.00 || 1000.00\n").to_stdout
-   
+    bank.create_bank_statement('10/01/2023',1000,nil, 1000)
+    expect{bank.printing_final_statement}.to output("date || credit || debit || balance \n10/01/2023 || 1000.00 ||  || 1000.00\n").to_stdout
+  end
+
+  it "should printing correct bank statement format with three bank statement(transaction) " do
+    bank = Bank.new
+    bank.create_bank_statement('10/01/2023',1000,nil, 1000)
+    bank.create_bank_statement('13/01/2023',2000,nil, 3000)
+    bank.create_bank_statement('14/01/2023',nil,500, 2500)
+    expect{bank.printing_final_statement}.to output("date || credit || debit || balance \n14/01/2023 ||  || 500.00 || 2500.00\n13/01/2023 || 2000.00 ||  || 3000.00\n10/01/2023 || 1000.00 ||  || 1000.00\n").to_stdout
   end
 end
