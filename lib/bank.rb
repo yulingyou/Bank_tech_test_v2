74857975cb2297ff9e0718459b95222ed3c98844
Daisy <ysiad926@gmail.com>
2023-01-26 14:32:04 +0000

    Bank class withdraw method test and drive

--- lib/bank.rb
+++ lib/bank.rb
@@ -8,8 +8,17 @@ class Bank
     update_balance(credit: credit)
   end
 
+  def withdraw(debit)
+    update_balance(debit: debit)
+  end
+
   def update_balance(credit: nil, debit: nil)
     @balance += credit if credit
+    @balance -= debit if debit
+  end
+
+  def balance
+    @balance
   end
 
 end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -3,7 +3,14 @@ require 'bank'
 RSpec.describe Bank do
   it "should return correct balance after deposit 1000" do
     bank = Bank.new
-    result = bank.deposit(1000)
-    expect(result).to eq(1000)
+    bank.deposit(1000)
+    expect(bank.balance).to eq(1000)
+  end
+
+  it "should return correct balance after deposit 1000 and withdraw 500" do
+    bank = Bank.new
+    bank.deposit(1000)
+    bank.withdraw(500)
+    expect(bank.balance).to eq(500)
   end
 end
